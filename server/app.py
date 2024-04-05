4a7815f361ac898eaf6eaace9828142640423780
Kipronokirui <victorkipronokirui2@gmail.com>
2024-04-05 20:22:54 +0300

    Assignment Completed

--- server/app.py
+++ server/app.py
@@ -21,7 +21,35 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
 
+    if earthquake:
+        response_body = earthquake.to_dict()
+        response_status = 200
+    else:
+        response_body = {
+            'message':f'Earthquake {id} not found.'
+        }
+        response_status = 404
+    
+    response = make_response(response_body, response_status)
+    return response
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def filter_by_magnitude(magnitude):
+    filtered_earthquakes = []
+    earthquakes=Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    for earthquake in earthquakes:
+        filtered_earthquakes.append(earthquake.to_dict())
+    response_body = {
+        'count':len(filtered_earthquakes),
+        'quakes':filtered_earthquakes
+    }
+    response = make_response(response_body, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
